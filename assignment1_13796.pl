b0ba002ffafdb67635499fab9e31480cb5de80f9
willd14469 <wd14469@my.bristol.ac.uk>
2018-02-15 15:46:28 +0000

    part 5.1 done.

--- assignment1_13796.pl
+++ assignment1_13796.pl
@@ -9,5 +9,14 @@ Q3([s,e,w,n]).
 
 Q4a([p(1, 4), p(2, 4), p(3, 4), p(4, 4), p(4, 3), p(3, 3), p(2, 3), p(1, 3), p(1, 2)).
 Q4b(L = [p(1, 4), p(2, 4), p(3, 4), p(4, 4), p(4, 3), p(3, 3), p(2, 3), p(1, 3), p(1, 2), p(1, 1), p(2, 1), p(2, 2), p(3, 2)).
- 
-use ; to create a requery.
+
+
+q5_corner_move() :-
+	ailp_start_position(p(X,Y)),
+	ailp_show_move(p(X,Y),p(4,4)),
+	ailp_show_move(p(4,4),p(4,1)),
+	ailp_show_move(p(4,1),p(1,1)),
+	ailp_show_move(p(1,1),p(X,Y)),
+	q5_corner_move().
+
+%use ; to create a requery.
