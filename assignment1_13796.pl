4e4e7de1898fb195057ef24a0ac143571b664dba
willd14469 <wd14469@my.bristol.ac.uk>
2018-02-15 15:19:02 +0000

    first

---
+++ assignment1_13796.pl
@@ -0,0 +1,13 @@
+candidate_number(13796).
+
+Q1(ailp_start_position(p(X,Y))).
+
+Q2a(new_pos(p(1,1),e,p(x1,y1))).
+Q2b(109).
+
+Q3([s,e,w,n]).
+
+Q4a([p(1, 4), p(2, 4), p(3, 4), p(4, 4), p(4, 3), p(3, 3), p(2, 3), p(1, 3), p(1, 2)).
+Q4b(L = [p(1, 4), p(2, 4), p(3, 4), p(4, 4), p(4, 3), p(3, 3), p(2, 3), p(1, 3), p(1, 2), p(1, 1), p(2, 1), p(2, 2), p(3, 2)).
+ 
+use ; to create a requery.
